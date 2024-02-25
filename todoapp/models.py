57db6f420960fcbfa5b596a38bbff27786f466d8
chedug <nuralyhug@gmail.com>
2024-02-25 02:19:51 +0100

    feat: add generic views and todo model

--- todoapp/models.py
+++ todoapp/models.py
@@ -1,6 +1,13 @@
 from django.db import models
-
+from datetime import date
 # Create your models here.
 
+
 class Todo(models.Model):
-    description = models.CharField()
+    title = models.CharField(max_length=100, blank=False)
+    description = models.TextField(blank=True)
+    date = models.DateField(blank=False, default=date.today)
+    completed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
